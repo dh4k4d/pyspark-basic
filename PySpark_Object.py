24502a500af78923e3351cee5b1bda32ff3667c6
dh4k4d <anandandnagar@gmail.com>
2023-11-23 15:47:48 +0000

    SparkObject function created

--- PySpark_Object.py
+++ PySpark_Object.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 
-object = SparkSession.Builder().master("local").appName("ObjectPySparkApp").getOrCreate()
+def SparkObject() :
+    object = SparkSession.Builder().master("local").appName("ObjectPySparkApp").getOrCreate()
+    return object
 
-print(object)
+if __name__ == '__main__':
+    print(SparkObject())
